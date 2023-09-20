13e812e40ee1395afe07200abfbfa61a77a9880a
adrianmark87 <adrianmarculescu.ing@gmail.com>
2023-09-20 14:02:36 +0200

    Password Forgotten + User CRUD done - Docker compose started

--- frontend/navigation/services/ApiHelper.jsx
+++ frontend/navigation/services/ApiHelper.jsx
@@ -17,8 +17,8 @@ const ApiHelper = async (
   };
 
   console.log(body);
-
-  return fetch(`http://192.168.1.77:5555${route}`, requestOptions);
+// Toujours vérifier que le url du front (Opening exp://172.20.10.3:8081 on Pixel_7_API_30) correspond avec ce qu'il y a dans le fetch
+  return fetch(`http://192.168.1.126:5555${route}`, requestOptions);
    
 };
 
