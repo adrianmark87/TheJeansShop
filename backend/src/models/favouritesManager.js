674b9cf4259daac3a8ff0a7724fedefebc6e18f0
adrianmark87 <adrianmarculescu.ing@gmail.com>
2023-10-02 11:09:12 +0200

    Favourites partiallt done

---
+++ backend/src/models/favouritesManager.js
@@ -0,0 +1,6 @@
+// findFavourites(id) {
+//     return this.connection.query(
+//       `select * from favourites join articles as f on f.article_id = article.id where user.id = ?`
+
+//     );
+//   }
